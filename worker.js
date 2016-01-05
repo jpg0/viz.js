9032993314841c8c2a443e6495505d5e7347bb5b
Jonathan Gilbert <jpg0@users.noreply.github.com>
2016-01-05 21:38:05 +1100

    added preprocessing

--- worker.js
+++ worker.js
@@ -1,6 +1,22 @@
 importScripts("bower_components/viz.js/viz.js");
 
+preprocess = function(str) {
+	var matches = str.match(/\/\* *replacements([\s\S]*)\*\//);
+	if(matches){
+		var replacements = JSON.parse(matches[1].trim());
+	} else {
+		var replacements = {};
+	}
+ 
+	for (var key in replacements) {
+  		if (replacements.hasOwnProperty(key)) {
+			str = str.replace(new RegExp('\\$'+key, 'g'), replacements[key]);
+  		}
+	}
+	return str;
+};
+
 onmessage = function(e) {
-  var result = Viz(e.data.src, e.data.options);
+  var result = Viz(preprocess(e.data.src), e.data.options);
   postMessage(result);
 }
